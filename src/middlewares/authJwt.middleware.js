a0bba68a8717484851e6ebba9500b83f2ab37d74
KasperNeth <okeowoa244@gmail.com>
2024-12-13 17:40:04 +0100

    Add: Auth middleware

---
+++ src/middlewares/authJwt.middleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken")
+const UserModel = require("../models/user.model")
+require("dotenv").config()
+
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.headers["x-access-token"];
+    if (!token) {
+      return res.status(403).send({ message: "No token provided!" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await UserModel.findById(decoded._id);
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    req.userId = decoded._id;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).send({ message: "Unauthorized!" });
+  }
+  return res.status(500).send({ message: error.message });
+
+  }
+
+}
+
+module.exports = verifyToken;
