b77d274e6c0c4de36b58234ab13a83a8c9c59447
KasperNeth <okeowoa244@gmail.com>
2024-12-13 16:37:04 +0100

    fix: validate error message

--- src/middlewares/validateRequest.middleware.js
+++ src/middlewares/validateRequest.middleware.js
@@ -3,8 +3,9 @@ const { validationResult } = require("express-validator");
 const validateRequest = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        const error = errors.array().map((err) => err.msg);
-        return res.status(400).json({ success: false, message: error });
+        const error = errors.array().map((err) => ({field: err.data,
+          message: err.msg}));
+        return res.status(400).json({ success: false, message: "Validation failed", error: error });
     }
     next();
 };
