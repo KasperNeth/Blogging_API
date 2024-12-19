69ed5a831a0bca13acd195688fe157803fc2e77e
KasperNeth <okeowoa244@gmail.com>
2024-12-19 15:24:01 +0100

    add: token timeout

---
+++ src/utils/Jwt.js
@@ -0,0 +1,25 @@
+const jwt =  require("jsonwebtoken");
+require("dotenv").config();
+
+const Tokengenerator = (user)=>{
+  const SECRET = process.env.JWT_SECRET;
+  const expiresIn = 60 * 60;
+    const Token = jwt.sign({
+        _id: user._id,
+        username: user.username,
+        email: user.email
+       
+    },
+    SECRET,
+    {expiresIn: expiresIn}
+
+   
+        
+    );
+    console.log(`generated token ${Token}`)
+    return Token
+  }
+
+
+
+  module.exports = Tokengenerator;
