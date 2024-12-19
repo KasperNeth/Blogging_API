f10eb7b8314d6e898ad7523b435d1d832570cf85
KasperNeth <okeowoa244@gmail.com>
2024-12-19 16:34:30 +0100

    add: Blog and public routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,14 @@
 const Router = require("express").Router;
 const AuthRoute = require("../routes/auth.routes");
+const BlogRoute = require("../routes/blog.routes");
+const PublicRoute = require("../routes/public.routes");
 
 
 const route = Router();
 
-route.use("/auth", AuthRoute);
+route.use("/v1/auth", AuthRoute);
+route.use("/api/v1/users", BlogRoute);
+route.use("/api/v1/posts", PublicRoute);
 
 
 
